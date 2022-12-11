0cbab64dcf32e06b251880da8a8cea4a4d5a6b67
Ryan Chiang <wsad71155@gmail.com>
2022-12-12 00:43:55 +0800

    1212

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,3 +2,28 @@ $(function() {
   console.log('Hello Bootstrap5');
 });
 
+var swiper = new Swiper(".coachCard-swiper", {
+  breakpoints: {
+    1: {
+      slidesPerView: 1.25,
+      spaceBetween: 10,
+    },
+    576: {
+      slidesPerView: 2.25,
+      spaceBetween: 10,
+    },
+    880: {
+      slidesPerView: 3.5,
+      spaceBetween: 15,
+    },
+    1200: {
+      slidesPerView: 4.5,
+      spaceBetween: 20,
+    },
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
