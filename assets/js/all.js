8cb06112d615eb1fbc02acd82bfa5c55300940da
Ryan Chiang <wsad71155@gmail.com>
2022-12-12 22:32:59 +0800

    Update 2022-12-12T14:32:56.343Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,8 +1,6 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
-});
+// 首頁_教練課程輪播
 var swiper = new Swiper(".coachCard-swiper", {
   breakpoints: {
     1: {
@@ -26,5 +24,11 @@ var swiper = new Swiper(".coachCard-swiper", {
     el: ".swiper-pagination",
     clickable: true
   }
-});
+}); // 千分位工具
+
+function toThousands(x) {
+  var parts = x.toString().split(".");
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return parts.join(".");
+}
 //# sourceMappingURL=all.js.map
